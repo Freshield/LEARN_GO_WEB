5e87e18da68c390c21411df78e641d5d70adcfee
freshield <yangyufresh@163.com>
2021-04-02 17:34:06 +0800

    bench test

---
+++ chp8/a1_unit_test/bench_test.go
@@ -0,0 +1,30 @@
+/*
+@Author: Freshield
+@Contact: yangyufresh@163.com
+@File: bench_test.py
+@Time: 2021-11-15 20:03
+@Last_update: 2021-11-15 20:03
+@Desc: None
+@==============================================@
+@      _____             _   _     _   _       @
+@     |   __|___ ___ ___| |_|_|___| |_| |      @
+@     |   __|  _| -_|_ -|   | | -_| | . |      @
+@     |__|  |_| |___|___|_|_|_|___|_|___|      @
+@                                    Freshield @
+@==============================================@
+*/
+package main
+
+import "testing"
+
+func BenchmarkDecode(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		decode("post.json")
+	}
+}
+
+func BenchmarkUnmarshal(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		unmarshal("post.json")
+	}
+}
