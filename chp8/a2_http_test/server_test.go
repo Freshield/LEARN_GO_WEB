d5e02388ba450b4c5f4b3b225f7157624f0bfa5c
freshield <yangyufresh@163.com>
2021-04-04 17:33:24 +0800

    http test

---
+++ chp8/a2_http_test/server_test.go
@@ -0,0 +1,36 @@
+/*
+@Author: Freshield
+@Contact: yangyufresh@163.com
+@File: server_test.py
+@Time: 2021-11-15 20:37
+@Last_update: 2021-11-15 20:37
+@Desc: None
+@==============================================@
+@      _____             _   _     _   _       @
+@     |   __|___ ___ ___| |_|_|___| |_| |      @
+@     |   __|  _| -_|_ -|   | | -_| | . |      @
+@     |__|  |_| |___|___|_|_|_|___|_|___|      @
+@                                    Freshield @
+@==============================================@
+*/
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestHandleGet(t *testing.T) {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/post/", handleRequest)
+
+	writer := httptest.NewRecorder()
+	request, _ := http.NewRequest("GET", "/post/1", nil)
+	mux.ServeHTTP(writer, request)
+
+	if writer.Code != 200 {
+		t.Errorf("Response code is %v", writer.Code)
+	}
+	t.Log(writer.Body.String())
+}
