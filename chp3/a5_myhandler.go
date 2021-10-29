ca6453ad4a5a01b389d5f9af2bdccebe85645a01
freshield <yangyufresh@163.com>
2021-10-29 15:42:51 +0800

    myhandler

---
+++ chp3/a5_myhandler.go
@@ -0,0 +1,36 @@
+/*
+@Author: Freshield
+@Contact: yangyufresh@163.com
+@File: a5_myhandler.py
+@Time: 2021-10-28 22:27
+@Last_update: 2021-10-28 22:27
+@Desc: None
+@==============================================@
+@      _____             _   _     _   _       @
+@     |   __|___ ___ ___| |_|_|___| |_| |      @
+@     |   __|  _| -_|_ -|   | | -_| | . |      @
+@     |__|  |_| |___|___|_|_|_|___|_|___|      @
+@                                    Freshield @
+@==============================================@
+*/
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type MyHandler struct{}
+
+func (h *MyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World!")
+}
+
+func main() {
+	handler := MyHandler{}
+	server := http.Server{
+		Addr: "127.0.0.1:9666",
+		Handler: &handler,
+	}
+	server.ListenAndServe()
+}
