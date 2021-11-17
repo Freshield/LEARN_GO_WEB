88bf2ff9bd1ac62627004dd1970ed8f1b15d7b3b
freshield <yangyufresh@163.com>
2021-11-17 17:32:34 +0800

    main test

---
+++ chp8/a1_unit_test/main_test.go
@@ -0,0 +1,45 @@
+/*
+@Author: Freshield
+@Contact: yangyufresh@163.com
+@File: main_test.py
+@Time: 2021-11-15 19:42
+@Last_update: 2021-11-15 19:42
+@Desc: None
+@==============================================@
+@      _____             _   _     _   _       @
+@     |   __|___ ___ ___| |_|_|___| |_| |      @
+@     |   __|  _| -_|_ -|   | | -_| | . |      @
+@     |__|  |_| |___|___|_|_|_|___|_|___|      @
+@                                    Freshield @
+@==============================================@
+*/
+package main
+
+import (
+	"testing"
+	"time"
+)
+
+func TestDecode(t *testing.T) {
+	post, err := decode("post.json")
+	if err != nil {
+		t.Error(err)
+	}
+	if post.Id != 1 {
+		t.Error("Wrong id, was expecting 1 but got", post.Id)
+	}
+	if post.Content != "Hello World!" {
+		t.Error("Wrong content, was expecting 'Hello World!' but got", post.Content)
+	}
+}
+
+func TestEncode(t *testing.T) {
+	t.Skip("Skipping encoding for now")
+}
+
+func TestLongRunningTest(t *testing.T) {
+	if testing.Short() {
+		t.Skip("Skipping long-running test in short mode")
+	}
+	time.Sleep(10 * time.Second)
+}
